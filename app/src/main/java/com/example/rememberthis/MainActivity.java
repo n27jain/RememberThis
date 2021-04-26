f9f061dc0d45c09f3f128b67e426eb5fe47d11a1
Naman Jain <n27jain@uwaterloo.ca>
2021-04-26 15:21:26 -0400

    Fixes Made: - Made UI adjustable - Shoudn't block the buttons anymore

--- app/src/main/java/com/example/rememberthis/MainActivity.java
+++ app/src/main/java/com/example/rememberthis/MainActivity.java
@@ -58,4 +58,21 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public void getInstanceID(){
+//        try {
+//            InstanceID instanceID = InstanceID.getInstance(this);
+//
+//            String token = instanceID.getToken(getString(R.string.gcm_defaultSenderId),
+//                    GoogleCloudMessaging.INSTANCE_ID_SCOPE, null);
+//
+//            Log.i(TAG, "GCM Registration Token: " + token);
+//
+//        }catch (Exception e) {
+//            Log.d(TAG, "Failed to complete token refresh", e);
+//        }
+
+    }
+
+
+
 }
