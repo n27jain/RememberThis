46db89723f4795763cb03f37e9f72d469b4864b0
Naman Jain <n27jain@uwaterloo.ca>
2021-04-25 21:36:23 -0400

    lost my changes. Making sure that this doesnt happen again

--- app/src/main/java/com/example/rememberthis/MainActivity.java
+++ app/src/main/java/com/example/rememberthis/MainActivity.java
@@ -2,13 +2,56 @@ package com.example.rememberthis;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final int REQUEST_VIDEO_CAPTURE = 1;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button recordButton =  findViewById(R.id.recordButton);
+        recordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakeVideoIntent();
+            }
+        });
+
     }
+
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        if (requestCode == REQUEST_VIDEO_CAPTURE && resultCode == RESULT_OK) {
+            Uri videoUri = intent.getData();
+            VideoView videoView =  findViewById(R.id.videoView);
+            videoView.setVideoURI(videoUri);
+        }
+    }
+
+
+    private void dispatchTakeVideoIntent() {
+        Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+        if (takeVideoIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takeVideoIntent, REQUEST_VIDEO_CAPTURE);
+        }
+    }
+
 }
